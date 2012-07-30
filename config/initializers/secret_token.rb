58034dddd03c75d117f5fd27d6c42bcbce2f64e7
David Liu <davidliu@David-Lius-MacBook-Pro.local>
2012-07-29 18:06:38 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HighEagleStudios::Application.config.secret_token = 'ba44bac439c794372938024d44190395517aeaaf3005802b4c331c8addf4677ae6f5b947e3252c1040d55a2d1abbafb73f22c092c3c9225e8db8cda25dd0c5fa'
