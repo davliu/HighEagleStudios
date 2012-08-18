777b624da37e0bce9bd2307741fa18a5158e6fa7
David Liu <davidliu@David-Lius-MacBook-Pro.local>
2012-08-17 23:41:39 -0700

    revised site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :check_uri
+
+  WWW_DOMAIN = 'www.higheaglestudios.com'
+  def check_uri
+    puts Rails.env
+    if (request.env['HTTP_HOST'] != WWW_DOMAIN) && Rails.env.production?
+      redirect_to "http://#{WWW_DOMAIN}", :status => 301
+    end
+  end
 end
